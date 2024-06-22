7f4786c33741eef0225b4adb5668331f14c015e3
Ildemar <168151340+k4minari@users.noreply.github.com>
2024-06-22 19:46:38 -0400

    creacion de la clase equipo y sus atributos

---
+++ Equipos.py
@@ -0,0 +1,18 @@
+class Equipo:
+# Constructor
+    def __init__(self, id, codigo, nombre, grupo):
+        
+        # Atributos
+        self.id = id
+        self.codigo = codigo
+        self.nombre = nombre
+        self.grupo = grupo
+        
+    def show(self):
+        print(f'''
+ID: {self.id}
+----------------------------
+Equipo: {self.nombre}
+Grupo: {self.grupo}
+Codigo FIFA: {self.codigo}
+____________________________''')
